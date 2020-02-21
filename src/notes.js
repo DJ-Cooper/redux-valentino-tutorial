dc9496f5e2328b4d41ecab9eeb14caee86e28d62
DJ Cooper <tuttle@metafaux.com>
2020-02-20 18:21:00 -0800

    create store and reducers directories

---
+++ src/notes.js
@@ -0,0 +1,8 @@
+/**
+ * The only way to change the state is by sending
+ * a signal to the store.
+ * This signal is an action that is "dispatched"
+ *
+ * Tutorial progress marker:
+ * "Create a directory for actions:"
+ */
